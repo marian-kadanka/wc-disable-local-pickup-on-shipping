f8ee67590d47f71ffaa80ead13f31f88ae683138
Marian Kadanka <marian.kadanka@gmail.com>
2017-03-22 14:04:58 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,28 @@
+=== Plugin Name ===
+Contributors: mariankadanka
+Donate link: https://github.com/marian-kadanka/
+Tags: local pickup, disable local pickup, ship to different address, different address shipping
+Requires at least: 3.0.1
+Tested up to: 4.7.3
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+An extension that disables WooCommerce built-in Local Pickup shipment method on checkout when a customer chooses to ship to a different address
+
+== Description ==
+
+This plugin turns off availability of WooCommerce built-in Local Pickup shipment method on the checkout page when a customer 
+chooses to ship an order to a different than billing address.
+
+Works with WooCommerce built-in Local Pickup shipment method only. There are no settings at all, just activate the plugin.
+
+== Installation ==
+
+1. Upload `wc-disable-local-pickup-on-shipping.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
---
+++ wc-disable-local-pickup-on-shipping.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Plugin Name:       WooCommerce Disable Local Pickup on Ship to Different Address
+ * Plugin URI:        https://wordpress.org/plugins/wc-disable-local-pickup-on-shipping
+ * Description:       An extension that disables WooCommerce built-in Local Pickup shipment method on checkout when a customer chooses to ship to a different address
+ * Version:           1.0.0
+ * Author:            Marian Kadanka
+ * Author URI:        https://github.com/marian-kadanka/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wc-disable-local-pickup-on-shipping
+ * Domain Path:       /languages
+ * GitHub Plugin URI: marian-kadanka/wc-disable-local-pickup-on-shipping
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+class WC_Disable_Local_Pickup_On_Shipping {
+
+	// could be the default, but it may lead to bad user experience on non ajax-enabled checkouts, when Local Pickup won't be available based on those settings, no matter if customer unchecks the "ship to different address" option
+	// apply_filters( 'woocommerce_ship_to_different_address_checked', get_option( 'woocommerce_ship_to_destination' ) === 'shipping' ? true : false );
+	public $ship_to_different_address = false;
+
+	function __construct() {
+
+		add_filter( 'woocommerce_cart_shipping_packages', array( $this, 'package_add_s2da_var' ) );
+		add_action( 'woocommerce_checkout_update_order_review', array( $this, 'checkout_update_s2da_flag' ) );
+		add_filter( 'woocommerce_shipping_local_pickup_is_available', array( $this, 'is_local_pickup_available' ), 10, 2 );
+
+	}
+
+	/**
+	 * Add variable indicating whether ship to different address is selected to the shipping package so that changing it forces a recalc
+	 * Don't process multi-package cart shipments
+	 */
+	function package_add_s2da_var( $packages ) {
+
+		if ( count( $packages ) === 1 ) {
+
+			// only one iteration, but rather retain key=>value relation for compatibility
+			foreach ( $packages as $package_index => $package ) {
+				$packages[ $package_index ]['wcdlpos_ship_to_different_address'] = $this->ship_to_different_address;
+			}
+		}
+
+		return $packages;
+	}
+
+	/**
+	 * Parse checkout form data and update the internal flag during an order review update event
+	 */
+	function checkout_update_s2da_flag( $post_data_str ) {
+		$post_data = array();
+		parse_str( $post_data_str, $post_data );
+
+		$this->ship_to_different_address = isset( $post_data['ship_to_different_address'] );
+	}
+
+	/**
+	 * Filter Local Pickup shipment method availability
+	 */
+	function is_local_pickup_available( $available, $package ) {
+		return $available && ! $this->ship_to_different_address;
+	}
+
+}
+
+new WC_Disable_Local_Pickup_On_Shipping();
